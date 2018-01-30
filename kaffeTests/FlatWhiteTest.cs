025d620b173256964a1e26eecd4a7fa0a7b47847
PeterKruhoffer <Peter.kruhoffer@gmail.com>
2018-01-30 13:33:14 +0100

    FlatWhite

---
+++ kaffeTests/FlatWhiteTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace kaffe.Tests
+{
+    [TestClass()]
+    public class FlatWhiteTest
+    {
+        [TestMethod()]
+        public void PrisTestFlatWhite()
+        {
+            //Arrange
+            var flatwhite = new FlatWhite();
+
+            //Act
+            int pris = flatwhite.Pris();
+
+            //assert
+            Assert.AreEqual(45, pris);
+        }
+
+        [TestMethod()]
+        public void StyrkeTestFlatWhite()
+        {
+            //Arrange
+            var flatWhite = new FlatWhite();
+            //Act
+            string styrke = flatWhite.Styrke();
+            //Assert
+            Assert.AreEqual("Middel", styrke);
+        }
+    }
+}
